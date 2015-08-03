cc230f7c91ea08659ec4cdd23727fdd89a862c4f
Kamil Mrzygłód <kmrzyglod@moneygram.com>
2015-08-03 10:19:14 +0200

    Initial commit

---
+++ Oxygenize/Oxygenize/Oxygenize.cs
@@ -0,0 +1,31 @@
+﻿namespace Oxygenize
+{
+    public class Oxygenize
+    {
+        public static Oxygenize<T> For<T>()
+        {
+            return new Oxygenize<T>();
+        } 
+    }
+
+    public class Oxygenize<T>
+    {
+        private GenerationStrategy _strategy;
+
+        /// <summary>
+        /// Sets the strategy used for data generation
+        /// </summary>
+        public Oxygenize<T> WithStrategy(GenerationStrategy strategy = GenerationStrategy.Random)
+        {
+            _strategy = strategy;
+            return this;
+        } 
+    }
+
+    public enum GenerationStrategy
+    {
+        Random,
+        Custom,
+        Mixed
+    }
+}
